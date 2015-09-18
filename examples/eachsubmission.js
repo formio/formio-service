537a321437f6f8cb325d972b1fcd4568ca01795c
Travis Tidwell <codertravist@gmail.com>
2015-09-17 20:24:23 -0500

    Fixing the urls.

--- examples/eachsubmission.js
+++ examples/eachsubmission.js
@@ -5,7 +5,7 @@ var Form = formio.Form;
 formio.authenticate('test@example.com', 'password').then(function() {
 
     // Create a new form instance.
-    var form = new Form('https://myapp.form.io/app/api/user');
+    var form = new Form('https://myapp.form.io/user');
 
     // Iterate through all the submissions.
     form.eachSubmission(function(submission) {
