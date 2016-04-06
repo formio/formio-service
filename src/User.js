6d2d9b85998a0b56c97b447c8324c1e17d7c37a1
Travis Tidwell <travis@form.io>
2016-04-05 21:42:48 -0500

    Added more form methods and reformated code.

--- src/User.js
+++ src/User.js
@@ -1,29 +1,31 @@
 var util = require('./util');
 var User = null;
-module.exports = function(config) {
-    if (User) { return User; }
-
-    User = function (email, pass) {
-        this.token = '';
-        this.email = email;
-        this.pass = pass;
-    };
-
-    /**
-     * Authenticate a new user.
-     * @returns {Request}
-     */
-    User.prototype.authenticate = function (form) {
-        form = form || '/user/login';
-        return util.request('post', config.formio + form + '/submission', {
-            data: {
-                'email': this.email,
-                'password': this.pass
-            }
-        }).then(function (res) {
-            this.token = res.headers['x-jwt-token'];
-        }.bind(this));
-    };
-
+module.exports = function (config) {
+  if (User) {
     return User;
+  }
+
+  User = function (email, pass) {
+    this.token = '';
+    this.email = email;
+    this.pass = pass;
+  };
+
+  /**
+   * Authenticate a new user.
+   * @returns {Request}
+   */
+  User.prototype.authenticate = function (form) {
+    form = form || '/user/login';
+    return util.request('post', config.formio + form + '/submission', {
+      data: {
+        'email': this.email,
+        'password': this.pass
+      }
+    }).then(function (res) {
+      this.token = res.headers['x-jwt-token'];
+    }.bind(this));
+  };
+
+  return User;
 };
--- src/util.js
+++ src/util.js
@@ -2,24 +2,24 @@ var superagent = require('superagent');
 var _ = require('lodash');
 var Q = require('q');
 module.exports = {
-    request: function(method, url, data, headers) {
-        var deferred = Q.defer();
-        method = method || 'get';
-        var request = superagent[method](url).set('Accept', 'application/json');
-        if (headers) {
-            _.each(headers, function (value, key) {
-                request = request.set(key, value);
-            });
-        }
-        if (data) {
-            request = request.send(data);
-        }
-        request.end(function (err, res) {
-            if (err) {
-                return deferred.reject(err);
-            }
-            deferred.resolve(res);
-        });
-        return deferred.promise;
+  request: function (method, url, data, headers) {
+    var deferred = Q.defer();
+    method = method || 'get';
+    var request = superagent[method](url).set('Accept', 'application/json');
+    if (headers) {
+      _.each(headers, function (value, key) {
+        request = request.set(key, value);
+      });
     }
+    if (data) {
+      request = request.send(data);
+    }
+    request.end(function (err, res) {
+      if (err) {
+        return deferred.reject(err);
+      }
+      deferred.resolve(res);
+    });
+    return deferred.promise;
+  }
 };
